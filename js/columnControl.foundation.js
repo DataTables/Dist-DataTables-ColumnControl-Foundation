bf1042b0ff78eec5c63fe820932339882278df4e
Allan Jardine <allan@datatables.net>
2026-05-07 15:35:15 +0000

    Updating package for DataTables 3

--- js/columnControl.foundation.js
+++ js/columnControl.foundation.js
@@ -1,6 +1,51 @@
-/*! Foundation styling wrapper for ColumnControl
- * © SpryMedia Ltd - datatables.net/license
+/*! ColumnControl Foundation styling 2.0.0-beta.1 for DataTables
+ * Copyright (c) SpryMedia Ltd - datatables.net/license
  */
+
+(function(factory){
+	if (typeof define === 'function' && define.amd) {
+		// AMD
+		define(['datatables.net-zf', 'datatables.net-columncontrol'], function (dt) {
+			return factory(window, document, dt);
+		});
+	}
+	else if (typeof exports === 'object') {
+		// CommonJS
+		var cjsRequires = function (root) {
+			if (! root.DataTable) {
+				require('datatables.net-zf')(root);
+			}
+
+			if (! window.DataTable.ColumnControl) {
+				require('datatables.net-columncontrol')(root);
+			}
+		};
+
+		if (typeof window === 'undefined') {
+			module.exports = function (root) {
+				if (! root) {
+					// CommonJS environments without a window global must pass a
+					// root. This will give an error otherwise
+					root = window;
+				}
+
+				cjsRequires(root);
+				return factory(root, root.document, root.DataTable);
+			};
+		}
+		else {
+			cjsRequires(window);
+			module.exports = factory(window, window.document, window.DataTable);
+		}
+	}
+	else {
+		// Browser
+		factory(window, document, window.DataTable);
+	}
+}(function(window, document, DataTable) {
+'use strict';
+
+
 
 DataTable.ColumnControl.content.dropdown.classes.container = [
 	'dtcc-dropdown',
@@ -10,3 +55,7 @@ DataTable.ColumnControl.content.dropdown.classes.container = [
 	'first-sub',
 	'vertical'
 ];
+
+
+return DataTable;
+}));
