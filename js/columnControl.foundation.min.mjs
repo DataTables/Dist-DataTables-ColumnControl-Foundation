0829179653ae68d24d1de2b6b8772f38004ca434
dtbuild <dtbuild@rigil>
2026-07-22 13:40:48 +0000

    9357823b84064d8c7144c51fcce9080bec43ef0c Release 2.0.0
    Sync to source repo @9357823b84064d8c7144c51fcce9080bec43ef0c

--- js/columnControl.foundation.min.mjs
+++ js/columnControl.foundation.min.mjs
@@ -1,4 +1,4 @@
-/*! ColumnControl Foundation styling 2.0.0-beta.1 for DataTables
+/*! ColumnControl Foundation styling 2.0.0 for DataTables
  * Copyright (c) SpryMedia Ltd - datatables.net/license
  */
 import DataTable from"datatables.net-zf";import ColumnControl from"datatables.net-columncontrol";DataTable.ColumnControl.content.dropdown.classes.container=["dtcc-dropdown","menu","submenu","is-dropdown-submenu","first-sub","vertical"];export default DataTable;
